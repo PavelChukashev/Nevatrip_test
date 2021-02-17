5611d610f8cd8e8f26de90075122f28bfdb3c200
PavelChukashev <60574922+PavelChukashev@users.noreply.github.com>
2021-02-17 15:24:43 +0300

    fix: change text in result

--- nevatrip/src/components/result.jsx
+++ nevatrip/src/components/result.jsx
@@ -14,7 +14,7 @@ export default function Result({
 
     return (
         <div>
-            <p>{`Вы выбрали ${count} билета по маршруту ${title} стоимостью ${count * price}р.`}</p>
+            <p>{`Вы выбрали ${count} билет(а) по маршруту ${title} стоимостью ${count * price}р.`}</p>
             <p>{`Теплоход ${title} отправляется в ${timeFromDate(time)}, а прибудет в ${increaseDateBy(time, 50).format(FORMATS.time)}.`}</p>
             {
                 secondTime && <p>{`Теплоход из В в А отправлется в ${timeFromDate(secondTime)}, прибудет в ${increaseDateBy(secondTime, 50).format(FORMATS.time)}`}</p>
